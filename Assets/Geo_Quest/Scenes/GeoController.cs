6c3391c5839cb2d6b1db7d7b0557b11c0e0653f3
lara0922 <laraiba0922@gmail.com>
2025-10-04 12:25:41 -0400

    C# classwork 2

--- Assets/Geo_Quest/Scenes/GeoController.cs
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -1,22 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
+
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
 public class Geo : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int speed = 5;
+
     // Start is called before the first frame update
-    string var0ne = "Hello ";
     void Start()
     {
-        string varTwo = "World";
-        Debug.Log(var0ne + varTwo);
-    }                                                                                                                                                                                                                  
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
-}
+    /*
+    if (Input.GetKeyDown(KeyCode.W))
+    {
+        transform.position += new Vector3(0, 1, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.S))
+    {
+        transform.position += new Vector3(0, -1, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.A))
+    {
+        transform.position += new Vector3(-1, 0, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.D))
+    {
+        transform.position += new Vector3(1, 0, 0);
+    }
+    */
+
+} 
