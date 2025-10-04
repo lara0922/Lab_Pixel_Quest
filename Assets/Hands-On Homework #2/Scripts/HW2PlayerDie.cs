31e2748bd2131ebe7e6462c5c210b4eb02c5d20f
lara0922 <laraiba0922@gmail.com>
2025-10-04 14:20:00 -0400

    hw #2

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerDie.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerDie.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HW2PlayerDie : MonoBehaviour
 {
+    public GameObject endPanel; 
+    private string Enemy = "Enemy"; 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Enemy)  
+        {
+            endPanel.SetActive(false); 
+            gameObject.SetActive(false);
+        }
+    }
 }
--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class HW2PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rgidbody2D;
+
+    private float _xSpeed; 
+    private float _ySpeed;
+
+    public float speed = 3; 
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+
+    private void Start()
+    {
+        _rgidbody2D = GetComponent<Rigidbody2D>();
+    }   
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(InputX); 
+        _ySpeed = Input.GetAxis(InputY);
+
+        _rgidbody2D.velocity = new Vector2(_xSpeed * 5, _ySpeed * 5) * speed;
+    }
+
 
 }
