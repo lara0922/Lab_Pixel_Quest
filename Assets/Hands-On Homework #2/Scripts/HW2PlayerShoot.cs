d0b3e4c0ca9e8661f174541374cd1f9c953b9c5b
lara0922 <laraiba0922@gmail.com>
2025-10-12 21:16:57 -0400

    handsonhw#2

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,84 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject preFabs;
+    public Transform bullet1Trash;
+    public Transform bullet1Spawn;
 
-}
+    public GameObject prefabs; 
+    public Transform bullet2Trash;
+    public Transform bullet2Spawn;
+
+
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+
+        {
+            TimerMethod();
+            Shoot1();
+            Shoot2();
+        }
+
+
+        {
+            if (!_canShoot)
+            {
+                _currentTime -= Time.deltaTime;
+
+                if (_currentTime < 0)
+                {
+                    _canShoot = true;
+                    _currentTime = Timer;
+
+                }
+
+            }
+        }
+    }
+
+
+    private void TimerMethod()
+    {
+        {
+            if (!_canShoot)
+            {
+                _currentTime -= Time.deltaTime;
+
+                if (_currentTime < 0)
+                {
+                    _canShoot = true;
+                    _currentTime = Timer;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private void Shoot1()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet1 = Instantiate(preFabs, bullet1Spawn.position, Quaternion.identity);
+
+            bullet1.transform.SetParent(bullet1Trash);
+        }
+    }
+    private void Shoot2()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
+        {
+            GameObject bullet2 = Instantiate(prefabs, bullet2Spawn.position, Quaternion.identity);
+
+            bullet2.transform.SetParent(bullet2Trash);
+
+        }
+
+    }
+} 
